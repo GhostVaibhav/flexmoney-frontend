ada0396d8fcfcbc916e75a4f9e40cab081abe8e9
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2023-12-17 16:01:47 +0530

    bug: Added range checks for the age input in the form

--- src/components/form.js
+++ src/components/form.js
@@ -18,7 +18,7 @@ function FormElt() {
                         <div class="sm:col-span-3">
                             <label for="age" class="block text-sm font-medium leading-6 text-white">Age</label>
                             <div class="mt-2">
-                                <input type="number" name="age" id="age" autocomplete="age" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                                <input min={18} max={65} type="number" name="age" id="age" autocomplete="age" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                             </div>
                         </div>
 
