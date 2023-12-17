7b64dc3d68cfffad3a73fe9dec290bc539cb6b57
Vaibhav Sharma <48472541+GhostVaibhav@users.noreply.github.com>
2023-12-17 20:47:46 +0530

    feat: Added retrieve and change function in the change batch page

--- src/components/Change.js
+++ src/components/Change.js
@@ -1,19 +1,81 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 
 function Change() {
-    const loading = useState(false);
-    useEffect(() => {
-        document.getElementById('enrollment-id').addEventListener('keydown', retrieve);
-    }, [retrieve]);
-    function retrieve() {
-        if(document.getElementById('enrollment-id').value.length === 64) {
-            // get the value of the current batch from the database
-            loading(true);
-            
-            loading(false);
+    async function retrieve() {
+        if (document.getElementById('enrollment-id').value.length === 64) {
+            var enrollmentID = document.getElementById('enrollment-id').value;
+            let response = { 'enrollmentID': enrollmentID };
+            response = JSON.stringify(response);
 
-            // set the value of the current batch to the input field
-            document.getElementById('current-batch').value = '6 - 7 AM'
+            try {
+                const resp = await fetch('http://localhost:3001/getb', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: response
+                });
+
+                const respJson = await resp.json();
+
+                if (respJson['error'] !== undefined) throw respJson['error']
+
+                if (respJson.batch === '6-7') {
+                    document.getElementById('current-batch').value = '6 - 7 AM'
+                }
+                else if (respJson.batch === '7-8') {
+                    document.getElementById('current-batch').value = '7 - 8 AM'
+                }
+                else if (respJson.batch === '8-9') {
+                    document.getElementById('current-batch').value = '8 - 9 AM'
+                }
+                else if (respJson.batch === '5-6') {
+                    document.getElementById('current-batch').value = '5 - 6 PM'
+                }
+            }
+            catch (err) {
+                document.getElementById('current-batch').value = "No such user exists";
+            }
+        }
+    }
+    async function change() {
+        if (document.getElementById('current-batch').value !== document.getElementById('new-batch').value) {
+            var enrollmentID = document.getElementById('enrollment-id').value;
+            var batch = document.getElementById('new-batch').value;
+            if(batch === '6 - 7 AM') {
+                batch = '6-7';
+            }
+            else if(batch === '7 - 8 AM') {
+                batch = '7-8';
+            }
+            else if(batch === '8 - 9 AM') {
+                batch = '8-9';
+            }
+            else if(batch === '5 - 6 PM') {
+                batch = '5-6';
+            }
+            let response = { 'enrollmentID': enrollmentID, 'batch': batch };
+            response = JSON.stringify(response);
+
+            try {
+                const resp = await fetch('http://localhost:3001/change', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: response
+                });
+
+                const respJson = await resp.json();
+
+                if (respJson['error'] !== undefined) throw respJson['error']
+
+                document.getElementById('dialog-box-1').classList.add('hidden');
+                document.getElementById('dialog-box-2').classList.remove('hidden');
+            }
+            catch (err) {
+                document.getElementById('new-batch').value = "No such user exists";
+            }
         }
     }
     return (
@@ -24,11 +86,14 @@ function Change() {
             </div>
 
             <div id="dialog-box-1" className='mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6'>
-                <div class="col-span-6">
+                <div class="col-span-5">
                     <label for="enrollment-id" class="block text-sm font-medium leading-6 text-white">Enrollment ID</label>
                     <div class="mt-2">
                         <input maxLength={64} type="text" name="enrollment-id" id="enrollment-id" autocomplete="enrollment-id" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                     </div>
+                </div>
+                <div className='flex w-full items-end'>
+                    <button onClick={retrieve} class="rounded-md w-full h-fit bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Check</button>
                 </div>
                 <div class="col-span-3">
                     <label for="current-batch" class="block text-sm font-medium leading-6 text-white">Current Batch</label>
@@ -48,7 +113,13 @@ function Change() {
                     </div>
                 </div>
 
-                <button class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Change</button>
+                <button onClick={change} class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Change</button>
+            </div>
+
+            <div id="dialog-box-2" className='hidden mt-10'>
+                <div className="border-b border-gray-900/10 pb-12">
+                    <h1 className="text-5xl font-semibold text-white">✔Batch changed!</h1>
+                </div>
             </div>
         </div>
     )
